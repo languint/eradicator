a42e4bb7070c3dd6e7865a7cff4915737a089075
languint <longuint@proton.me>
2025-11-18 20:09:58 +0000

    feat: initialize axum server in eradicator

--- eradicator/src/main.rs
+++ eradicator/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::{Router, get};
+
+#[tokio::main]
+pub async fn main() -> Result<(), String> {
+    let app: Router = Router::new().route("/", get(|| async { "eradicator" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .map_err(|e| format!("Failed to bind to port 3000: {e}"))?;
+
+    axum::serve(listener, app)
+        .await
+        .map_err(|e| format!("Failed to serve app: {e}"))?;
+
+    Ok(())
 }
